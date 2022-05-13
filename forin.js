e89e5e36382a01a8cc90f1a8d74fa51f3848a5d1
김종찬 <kimjongchan@gimjongchan-ui-MacBookAir.local>
2022-05-13 12:50:20 +0900

    version4

--- forin.js
+++ forin.js
@@ -1,18 +1,27 @@
 const member = {
     firstMember: {
         name: '김종찬',
+        id: 'kjc',
+        password: 1234,
         adress: '시흥시',
         phone: 010 - 1234 - 5678,
         age: 29,
     },
     secondMember: {
         name: '홍길동',
+        id: 'hgd',
+        password: 5678,
         adress: '서울시',
         phone: 010 - 5678 - 1234,
         age: 30
     }
 }
 
+
+
+/* 회원정보 삭제*/
+
+/*
 function delMember() {
     const userName = document.querySelector("#name").value;
     for (let key in member) {
@@ -24,3 +33,4 @@ function delMember() {
     }
     alert('회원정보가 없습니다.');
 }
+*/
